0c0e2662c0c086273ac7ce2a3e07aa76aa536625
ant <antdwhitehead@gmail.com>
2020-05-09 14:09:10 +0100

    sorted out data

---
+++ database/factories/PoemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Poem;
+use Faker\Generator as Faker;
+
+$factory->define(Poem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/PoemSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use JeroenZwart\CsvSeeder\CsvSeeder;
+
+class PoemSeeder extends CsvSeeder
+{
+
+    public function __construct()
+    {
+        $this->file = '/database/seeds/csvs/poems.csv';
+        $this->delimiter = ',';
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    parent::run();
+    }
+}
